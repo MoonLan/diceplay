28a44a170904e9ebef95f1d945914163b2741054
jhes <ooxfordck@gmail.com>
2018-08-29 23:41:44 +0800

    fixed the issues

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -120,7 +120,7 @@ export async function setContext(app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {"WS_URL":"wss://gydice.herokuapp.com/ws","user_id":"","clienthash":"","serverhash":"","userhash":"","nonce":"","wallets":[{"id":1,"currency":"btc","amount":0},{"id":2,"currency":"eth","amount":0},{"id":3,"currency":"bnb","amount":0}]}
+      env: {"WS_URL":"wss://gydice.herokuapp.com/ws","BASE_URL":"https://gydice.herokuapp.com/","user_id":"","clienthash":"","serverhash":"","userhash":"","nonce":"","wallets":[{"id":1,"currency":"btc","amount":0},{"id":2,"currency":"eth","amount":0},{"id":3,"currency":"bnb","amount":0}]}
     }
     // Only set once
     if (context.req) app.context.req = context.req
